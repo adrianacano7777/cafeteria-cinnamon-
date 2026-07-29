a18428bcc8b1e7c05a25e2a9f4a4702d0d6c4442
Manny007 <mannytapia715@gmail.com>
2026-07-29 10:20:29 -0600

    Se corrigio el fondo blanco a nuestro fondo cafe en la pestana mi perfil

--- php/perfil.php
+++ php/perfil.php
@@ -16,8 +16,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo $titulo_pagina; ?></title>
   <link rel="icon" href="../img/icono-pestana.png" type="image/png">
-  <link rel="stylesheet" href="../css/perfil.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/perfil.css">
 </head>
 
 <body>
